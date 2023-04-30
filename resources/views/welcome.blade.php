068f25f4ccc3758ae1b76457c62279d5c70cafba
Hung Duong Quang <qhungg289@protonmail.com>
2023-04-30 17:31:17 +0700

    thêm và sử dụng layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-    @vite('resources/css/app.css')
-</head>
-
-<body class="antialiased">
+<x-layout>
+    <x-slot name="title">Home</x-slot>
     <h1>Bookmark Manager</h1>
-</body>
-
-</html>
+</x-layout>
