1d910a5b6fe294fca9d9550c7e2899b81fe472b2
Hung Duong Quang <qhungg289@protonmail.com>
2023-05-28 10:00:05 +0700

    update search view

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -35,7 +35,14 @@
         @endisset
         @isset($tags)
             @foreach ($tags as $tag)
-                <a href="{{ route('tags.show', ['tag' => $tag]) }}" class="block py-3 hover:underline">#{{ $tag->name }}</a>
+                <div class="flex items-center gap-2 py-3 group">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5" />
+                    </svg>
+                    <a href="{{ route('tags.show', ['tag' => $tag]) }}" class="hover:underline">
+                        {{ $tag->name }}
+                    </a>
+                </div>
             @endforeach
         @endisset
     </div>
