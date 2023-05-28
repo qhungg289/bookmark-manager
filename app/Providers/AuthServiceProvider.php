3545b2ab63885e8f1003898e13d49fec6f32e4d9
Hung Duong Quang <qhungg289@protonmail.com>
2023-05-28 08:47:19 +0700

    update AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,10 +6,12 @@ namespace App\Providers;
 
 use App\Models\Bookmark;
 use App\Models\Folder;
+use App\Models\User;
 use App\Policies\BookmarkPolicy;
 use App\Policies\FolderPolicy;
 use App\Policies\TagPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -29,6 +31,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('admin', function (User $user) {
+            return $user->role == 'admin';
+        });
     }
 }
